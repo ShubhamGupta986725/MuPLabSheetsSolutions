1dbafa1deafb3d52310706c8936698015ba8ead3
ShubhamGupta986725 <95865181+ShubhamGupta986725@users.noreply.github.com>
2023-03-24 01:02:47 +0530

    Create Lab7Task2_Lab7Task1ButWithVariableInputSize.asm

---
+++ Lab7Task2_Lab7Task1ButWithVariableInputSize.asm
@@ -0,0 +1,144 @@
+.model tiny
+.data
+    ; Data section contains the messages, the correct username, and password for comparison.
+
+    usn1    db "anub@g.com$" 
+    ; Correct username for comparison
+
+    max1    db 20 
+    ; Maximum length for input
+
+    act1    db ?
+    ; Placeholder for action
+
+    inp1    db 20 dup ("$") 
+    ; Buffer to store user's input for username
+
+    fname1    db "username.txt", 0 
+    ; Correct username for comparison
+
+    handle1 dw ?
+
+    fname2    db "password.txt", 0 
+    ; Correct username for comparison
+
+    handle2 dw ?
+
+    msg2    db "enter 5 character long password:$" 
+    ; Message 2: Prompt to enter the password
+
+    pass1   db "oscar" 
+    ; Correct password for comparison
+
+    max2 db 30
+
+    inp2    db 30 dup ("$") 
+    ; Buffer to store user's input for password
+
+    msg3    db "hello $" 
+    ; Message 3: Greeting message when both inputs are correct
+
+    msg4    db "wrong username$" 
+    ; Message 4: Wrong username input
+
+    msg5    db "wrong password$" 
+    ; Message 5: Wrong password input
+    
+    msg6    db "How Long is your Username?: $"
+
+    msg7    db "How Long is your Password?: $"
+
+    nline   db 0ah, 0dh, "$" 
+    ; New line characters
+.code
+.startup
+; Take input from the username.txt and store it in inp1.
+    mov ah, 3dh
+    mov al, 0h
+    lea dx, fname1
+    int 21h
+    mov handle1, ax
+
+    mov ah, 3fh
+    mov bx, handle1
+    mov cl, max1
+    mov ch, 0
+    lea dx, inp1
+    int 21h
+
+    mov ah, 3eh
+    int 21h
+; Compare the username in username.txt with the stored username.
+    cld
+    lea di, inp1
+    lea si, usn1
+    mov cx, 10
+    repe cmpsb
+    jcxz l1
+; If the username is incorrect, display the "wrong username" message and exit.
+    lea dx, nline
+    mov ah, 09h
+    int 21h
+    lea dx, msg4
+    mov ah, 09h
+    int 21h
+    mov ah, 4ch
+    int 21h
+
+; If username is correct, Prompt the user to input the size of the password
+    lea dx, msg7
+    mov ah, 09h
+    int 21h
+; Take input from user about how long the password is
+    mov ah, 01h
+    int 21h
+    mov cl, al
+    mov ch, 0h
+; If the username is correct, read password from password.txt and store it in inp2
+l1: mov ah, 3dh
+    mov al, 0h
+    lea dx, fname2
+    int 21h
+    mov handle2, ax
+
+    mov ah, 3fh
+    mov bx, handle2
+    mov cl, max2
+    mov ch, 0
+    lea dx, inp2
+    int 21h
+
+    mov ah, 3eh
+    int 21h
+; Compare the entered password with the stored password.
+    cld
+    mov cx, 6
+    lea di, inp2
+    lea si, pass1
+    repe cmpsb
+    jcxz l3
+; If the password is incorrect, display the "wrong password" message and exit.
+    lea dx, nline
+    mov ah, 09h
+    int 21h
+    lea dx, msg5
+    mov ah, 09h
+    int 21h
+    mov ah, 4ch
+    int 21h
+; If the password is correct, display the greeting message an the username.
+    
+l3: lea dx, nline
+    mov ah, 09h
+    int 21h
+    lea dx, msg3
+    mov ah, 09h
+    int 21h
+    lea dx, usn1
+    mov ah, 09h
+    int 21h
+    lea dx, nline
+    mov ah, 09h
+    int 21h
+.exit
+end
